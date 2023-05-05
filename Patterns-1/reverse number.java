52ffed01aa0bcb03f8276ee27f630a4aadb094ea
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-05-05 18:54:07 +0530

    Add files via upload

---
+++ Patterns-1/reverse number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        int m;
+        for(int i=1;i<=N;i++)
+        {            m=i;
+                     for(int j=0;j<i;j++)
+                    {    
+                        System.out.print(m);
+                        m--;
+                    }   
+        System.out.println("");
+        }
+
+		
+	}
+
+}
