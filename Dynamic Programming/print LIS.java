a132d843e5f92a4a004cc21e2b18f238481a28c9
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-08-18 20:45:01 +0530

    Add files via upload

---
+++ Dynamic Programming/print LIS.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class TUF{
+static int longestIncreasingSubsequence(int arr[], int n){
+    
+    int dp[][]=new int[n+1][n+1];
+    
+    for(int ind = n-1; ind>=0; ind --){
+        for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+            
+            int notTake = 0 + dp[ind+1][prev_index +1];
+    
+            int take = 0;
+    
+            if(prev_index == -1 || arr[ind] > arr[prev_index]){
+                
+                take = 1 + dp[ind+1][ind+1];
+            }
+    
+            dp[ind][prev_index+1] = Math.max(notTake,take);
+            
+        }
+    }
+    
+    return dp[0][0];
+}
