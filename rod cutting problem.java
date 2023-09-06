6117b27b1463e714e0fbb5ab48f2bca5a2ceef3d
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-09-06 22:14:38 +0530

    Add files via upload

---
+++ rod cutting problem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+
+	public static int MaxRod(int price[], int ind, int n, int[][] dp){
+		if(ind == 0){
+			return n*price[0];
+		}
+		if(dp[ind][n] != -1){
+			return dp[ind][n];
+		}
+		int notTake = 0 + MaxRod(price, ind-1, n, dp);
+		int take = Integer.MIN_VALUE;
+		int rodlength = ind + 1;
+		if(rodlength <= n){
+			take = price[ind] + MaxRod(price, ind, n - rodlength, dp);
+		}
+		return dp[ind][n] = Math.max(notTake, take);
+	}
+	public static int cutRod(int price[], int n) {
+		int[][] dp = new int[n][n+1];
+		for(int[] row:dp)
+		Arrays.fill(row,-1);
+		return MaxRod(price, n-1, n, dp);
+		
+	}
+}
