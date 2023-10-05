2380be0c874c9f7754a12753577b6a0790914aae
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-10-05 22:36:14 +0530

    Add files via upload

---
+++ DSA SHEET/multiplication table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int i = 1;
+        while(i<=10)
+        {
+            
+            System.out.println(i*n);
+            i++;
+        }
+        
+
+	}
+}
