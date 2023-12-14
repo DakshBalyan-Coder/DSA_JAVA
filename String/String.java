4a2cd3a9318aed0e582c101a432ec85529eecca8
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-12-14 19:30:24 +0530

    Add files via upload

---
+++ String/String.java
@@ -0,0 +1,16 @@
+class Solution {
+       
+       public static void print(String s){
+           for(int i = 0; i<s.length(); i++){
+               System.out.print(s.charAt(i));
+           }
+       }
+
+    public static void main(String[] args) {
+        // Write your code here
+       
+        String s = "abcdefgh";
+       print(s);
+       
+    }
+}
